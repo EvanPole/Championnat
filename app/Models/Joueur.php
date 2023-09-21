6e14377028bec408b12351f1ed42470b07492fc1
Evan <suireevan@gmail.com>
2023-09-21 10:10:19 +0200

    model for migration

--- app/Models/Joueur.php
+++ app/Models/Joueur.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Joueur extends Model
 {
     use HasFactory;
+    public function equipe()
+    {
+        return $this->belongsTo(Equipe::class);
+    }
 }
