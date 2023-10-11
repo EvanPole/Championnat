2f4f837dc0729abf588ae0b6259d9498ea9241d3
Evan <suireevan@gmail.com>
2023-10-11 19:20:13 +0200

    rework page add responsive

--- resources/views/championnat.blade.php
+++ resources/views/championnat.blade.php
@@ -1,45 +1,44 @@
 @extends('layout.navbar')
 @section('content')
 
-    <h1>Matches</h1>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Domicile</th>
-                <th scope="col">Visiteur</th>
-                <th scope="col">date</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            @forelse ($matche as $matches)
-                <tr>
-
-                    @foreach ($equipe as $equipes)
-                        @if ($matches->domicile == $equipes->id)
-                            <td>{{ $equipes->ville }}</td>
-                        @endif
-                    @endforeach
-
-                    @foreach ($equipe as $equipes)
-                        @if ($matches->visiteur == $equipes->id)
-                            <td>{{ $equipes->ville }}</td>
-                        @endif
-                    @endforeach
-                    <td>{{ $matches->date }}</td>
+    <h1 class="text-center">Matches</h1>
+    <div class="table-responsive m-5">
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr class="bg-primary text-white">
+                    <th scope="col">Domicile</th>
+                    <th scope="col">Visiteur</th>
+                    <th scope="col">Date</th>
                 </tr>
-            @empty
+            </thead>
+            <tbody>
+                @forelse ($matche as $matches)
+                    <tr>
+                        @foreach ($equipe as $equipes)
+                            @if ($matches->domicile == $equipes->id)
+                                <td>{{ $equipes->ville }}</td>
+                            @endif
+                        @endforeach
+                        @foreach ($equipe as $equipes)
+                            @if ($matches->visiteur == $equipes->id)
+                                <td>{{ $equipes->ville }}</td>
+                            @endif
+                        @endforeach
+                        <td>{{ $matches->date }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="3">
+                            <div class="alert alert-secondary" role="alert">
+                                Ha, Flut il n'y a pas de matches !
+                            </div>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 
-                <div class="alert alert-secondary" role="alert">
-                    Ha, Flut il n'y a pas de matches !
-                </div>
-            @endforelse
-
-        </tbody>
-    </table>
-    <h1>Equipes du championnats</h1>
-
-<h1>soon</h1>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
